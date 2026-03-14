12792883dd9f992fd83853efd6723f8f062880e3
rasikaitankar <rasikaitankar21105@gmail.com>
2026-03-14 15:54:43 +0530

    Adding new fortune logic

--- src/main/java/com/rcoem/devops/DevopsApplication.java
+++ src/main/java/com/rcoem/devops/DevopsApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsApplication.class, args);
+//	public static void main(String[] args) {
+//		SpringApplication.run(DevopsApplication.class, args);
+//	}
+
+		public static void main(String[] args) {
+			String[] fortunes = {"You will pass this lab!", "Success is in your Jenkinsfile.", "Error 404: Fortune not found."};
+			int random = (int) (Math.random() * fortunes.length);
+			System.out.println("THE ORACLE SAYS: " + fortunes[random]);
+		}
 	}
 
-}
